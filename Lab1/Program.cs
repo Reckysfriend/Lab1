86f045610c01008b642791bbc5201e11818440dc
recky <recky@RECKY>
2024-11-18 10:42:31 +0100

    Created a basic Menu function and a visual display for the menu

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,9 +2,14 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MainMenu();
+        }
+        static void MainMenu()
+        {
+            Console.WriteLine("\tWelcome to our Main Menu\n\n \t[1] Option 1\n\t[2] Option 2\n\t[3] Option 3\n\n\t[9] Quit");
         }
     }
 }
