c2d19c04c914fadda85152d846d772c34fe9127f
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-11-21 10:54:26 +0100

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,10 +2,20 @@
 {
     internal class Program
     {
+
         static void Main()
         {
-            //string newyorktostockholm = Console.ReadLie();
-            //string stockholmtonewyork = Console.ReadLine();
+            int newYorkDepartureTimeHour = 10;
+            int newYorkDepartureTimeMin = 10;
+            int stockholmDepartureTimeHour = 14;
+            int stockholmDepartureTimeMin = 03;
+            string stockholmDepartureTimeMinStr = stockholmDepartureTimeMin.ToString("00");
+
+            Console.WriteLine(newYorkDepartureTimeHour);
+            Console.WriteLine(newYorkDepartureTimeMin);
+            Console.WriteLine(stockholmDepartureTimeHour);
+            Console.WriteLine(stockholmDepartureTimeMinStr);
+            
             bool menu = true;
             while (menu)
             {
@@ -16,12 +26,15 @@
                 {
                     case "1":
                         Console.Clear();
-                        Console.WriteLine("S -> NY");
+                        Console.WriteLine($"\tStockholm to New York\n\nThe Plane departs at: {newYorkDepartureTimeHour}:{newYorkDepartureTimeMin}");
+                        Console.WriteLine($"\tStockholm To New York\n\nThe Plane arrives at: {newYorkDepartureTimeHour}{+}{7}:{newYorkDepartureTimeMin}"
+                        Console.ReadLine();
                         menu = false;
                         break;
                     case "2":
                         Console.Clear();
-                        Console.WriteLine("NY -> S");
+                        Console.WriteLine($"\tStockholm to New York\n\nThe Plane departs at: {stockholmDepartureTimeHour}:{stockholmDepartureTimeMinStr}");
+                        Console.ReadLine();
                         menu = false;
                         break;
                     default:
