8997acaa054e57f6d4d66d21900d7e31c6f03760
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-11-21 13:38:05 +0100

    Added a lot of shit
    Added most of the assignment

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,16 +8,21 @@
             int newYorkDepartureTimeHour = 10;
             int newYorkDepartureTimeMin = 10;
             int stockholmDepartureTimeHour = 14;
-            int stockholmDepartureTimeMin = 03;
+            int stockholmDepartureTimeMin = 3;
             int travelTimeHour = 7;
+            int travelTimeMin = 25;
             int timeZoneDifference = 6;
             string stockholmDepartureTimeMinStr = stockholmDepartureTimeMin.ToString("00");
             int newYorkArrivalTimeHour = ((newYorkDepartureTimeHour * 60) + (travelTimeHour * 60) - (timeZoneDifference * 60)) / 60;
-            Console.WriteLine(newYorkDepartureTimeHour);
-            Console.WriteLine(newYorkDepartureTimeMin);
-            Console.WriteLine(stockholmDepartureTimeHour);
-            Console.WriteLine(stockholmDepartureTimeMinStr);
-            /*
+            int newYorkArrivalTimeMin = newYorkDepartureTimeMin + travelTimeMin;
+            int stockholmArrivalTimeHour = ((stockholmDepartureTimeHour * 60) + (travelTimeHour * 60) + (timeZoneDifference * 60)) / 60;
+            
+            if (stockholmArrivalTimeHour >= 24)
+            {
+                stockholmArrivalTimeHour = stockholmArrivalTimeHour - 24;
+            }
+            string stockholmArrivalTimeHourStr = stockholmArrivalTimeHour.ToString("00");
+            int stockholmArrivalTimeMin = stockholmDepartureTimeMin + travelTimeMin;
             bool menu = true;
             while (menu)
             {
@@ -28,14 +33,15 @@
                 {
                     case "1":
                         Console.Clear();
-                        Console.WriteLine($"\tStockholm to New York\n\nThe Plane departs at: {newYorkDepartureTimeHour}:{newYorkDepartureTimeMin}");
-                        Console.WriteLine($"\tStockholm To New York\n\nThe Plane arrives at: {newYorkDepartureTimeHour}:{newYorkDepartureTimeMin}");
+                        Console.WriteLine($"\tStockholm to New York\n\nThe plane departs at: {newYorkDepartureTimeHour}:{newYorkDepartureTimeMin}" +
+                            $"\nThe plane arrives at: {newYorkArrivalTimeHour}:{newYorkArrivalTimeMin}");
                         Console.ReadLine();
                         menu = false;
                         break;
                     case "2":
                         Console.Clear();
-                        Console.WriteLine($"\tStockholm to New York\n\nThe Plane departs at: {stockholmDepartureTimeHour}:{stockholmDepartureTimeMinStr}");
+                        Console.WriteLine($"\tStockholm to New York\n\nThe Plane departs at: {stockholmDepartureTimeHour}:{stockholmDepartureTimeMinStr}" +
+                        $"\nThe plane arrives at: " + stockholmArrivalTimeHourStr +$":{stockholmArrivalTimeMin}");
                         Console.ReadLine();
                         menu = false;
                         break;
@@ -44,7 +50,8 @@
                         Console.WriteLine("Please enter a valid choice");
                         break;
                 }
-            }*/
+            }
         }
     }
 }
+ 
