c80386351244d1eabdc333f2e2b8c17d642eaec9
The gungus <mikaelsson.viggo@gmail.com>
2024-11-21 10:03:55 +0100

    Added Switch for Menu

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //string newyorktostockholm = Console.ReadLie();
+            //string stockholmtonewyork = Console.ReadLine();
+            bool menu = true;
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("\n\t\tFlight Table \n\n\t[1] Stockholm - New York\n\t[2] New York -> Stockholm");
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("S -> NY");
+                        menu = false;
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("NY -> S");
+                        menu = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please enter a valid choice");
+                        break;
+                }
+            }
         }
     }
 }
