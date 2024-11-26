0bb88376ba973c50db7d857c25530497f66459ce
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-11-26 09:06:45 +0100

    Added some pretty & removed un-needed function
    Added line spacing with "*"
    Removed 24h converted since we never achive the number in this assingment

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -12,7 +12,8 @@ namespace Lab1
             int newYorkDepartureTimeMin = 10;
             int stockholmDepartureTimeHour = 14;
             int stockholmDepartureTimeMin = 3;
-            int timeZone = 6;        
+            int timeZone = 6;
+            string lineSeperator = "\t*****************************************";
             /*
             Vi deklarerar och instansier en bool för att 
             använda den som condition i våran while-loop, 
@@ -25,8 +26,8 @@ namespace Lab1
                 vi använder en while-loop för att vi vill at programmet 
                 ska fortsätta tills användaren väljer att stänga av den,
                  */
-                Console.WriteLine("\n\t\tFlight Table \n\n\t[1] Stockholm -> New York\n\t[2] New York -> Stockholm\n\t[3] End Program");
-                Console.Write("\n\tCHOICE: ");
+                Console.WriteLine($"{lineSeperator}\n\t\t\tFlight Table \n\n\t\t[1] Stockholm -> New York\n\t\t[2] New York -> Stockholm\n\t\t[3] End Program");
+                Console.Write("\n\t\tCHOICE: ");
                 /*
                  vi tar in en string-input från användaren och försöker 
                 konvertera den till en int32
@@ -40,13 +41,13 @@ namespace Lab1
                 {
                     case 1:
                         Console.Clear();
-                        Console.WriteLine("\tStockholm to New York\n");
-                        FlightCalculation(stockholmDepartureTimeHour, stockholmDepartureTimeMin, -timeZone); 
+                        Console.WriteLine($"{lineSeperator}\n\t\tStockholm to New York\n");
+                        FlightCalculation(stockholmDepartureTimeHour, stockholmDepartureTimeMin, -timeZone,lineSeperator); 
                         break;
                     case 2:
                         Console.Clear();
-                        Console.WriteLine("\tNew York to Stockholm\n");
-                        FlightCalculation(newYorkDepartureTimeHour, newYorkDepartureTimeMin, timeZone);
+                        Console.WriteLine($"{lineSeperator}\n\t\tNew York to Stockholm\n");
+                        FlightCalculation(newYorkDepartureTimeHour, newYorkDepartureTimeMin, timeZone,lineSeperator);
                         break;
                     case 3:
                         menu = false;
@@ -59,7 +60,7 @@ namespace Lab1
                 }
             }
         }
-        static void FlightCalculation(int departureHour, int departureMin, int timeZone)
+        static void FlightCalculation(int departureHour, int departureMin, int timeZone, string line)
         {
             string departureHourStr = departureHour.ToString("00");
             string departureMinStr = departureMin.ToString("00");
@@ -67,16 +68,12 @@ namespace Lab1
             int travelTimeMin = 25;
 
             int arrivalTimeHour = (((departureHour * 60) + (travelTimeHour * 60) + (timeZone * 60)) / 60);
-            if (arrivalTimeHour >= 24)
-            {
-                arrivalTimeHour -= 24;
-            }
             int arrivalTimeMin = departureMin + travelTimeMin;
             string arrivalTimeHourStr = arrivalTimeHour.ToString("00");
             string arrivalTimeMinStr = arrivalTimeMin.ToString("00");
 
-            Console.WriteLine($"\n\tThe plane departs at: {departureHourStr}:{departureMinStr}" +
-            $"\n\tThe plane arrives at: {arrivalTimeHourStr}:{arrivalTimeMinStr}");
+            Console.WriteLine($"{line}\n\t\tThe plane departs at: {departureHourStr}:{departureMinStr}" +
+            $"\n\t\tThe plane arrives at: {arrivalTimeHourStr}:{arrivalTimeMinStr}\n");
 
         }
     }
